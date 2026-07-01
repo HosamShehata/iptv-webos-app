3e00a245bc377d843d2ce696632f478fad667654
HosamShehata <90468607+HosamShehata@users.noreply.github.com>
2026-07-01 07:25:52 +0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3,9 +3,23 @@ const video = document.getElementById("video");
 const channel = JSON.parse(localStorage.getItem("current"));
 
 if (channel) {
-  video.src = channel.url;
-  video.play();
 
-  // حفظ آخر قناة
-  localStorage.setItem("last", JSON.stringify(channel));
+  async function initPlayer() {
+
+    const player = new shaka.Player(video);
+
+    player.addEventListener('error', (e) => {
+      console.error('Error', e);
+    });
+
+    try {
+      await player.load(channel.url);
+      video.play();
+    } catch (e) {
+      console.error("Load failed", e);
+    }
+
+  }
+
+  initPlayer();
 }
